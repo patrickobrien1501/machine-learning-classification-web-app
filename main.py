96c4f03f93939b427d8f9674f782c15b9bd1d79c
patrickobrien1501 <jpobrien@gmx.com>
2022-04-05 23:31:20 +0200

    function documentation and clean up

--- main.py
+++ main.py
@@ -17,12 +17,17 @@ st.title("Streamlit ML classification Web App")
 st.write("Which one is the best classifier?")
 
 dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine"))
-
 classifier_name = st.sidebar.selectbox("Select Classifier", ("KNN", "SVM", "Random Forest"))
 
-# function to get the datasets
+# function definitions
 def get_dataset(dataset_name):
-    # load datasets depending on user choice
+    '''
+    function to load datasets depending on user choice
+    Input: dataset_name obtained from sidebar dropdown menu
+    Outputs: Training data X and labeled output vector y
+
+    '''
+    
     if dataset_name == "Iris":
         data = datasets.load_iris()
     elif dataset_name == "Breast Cancer":
@@ -35,11 +40,13 @@ def get_dataset(dataset_name):
     y = data.target
     return X, y
 
-X, y = get_dataset(dataset_name)
-st.write("shape of dataset", X.shape)
-st.write("number of classes", len(np.unique(y)))
-
 def add_parameter_ui(clf_name):
+    '''
+    Function to add classifier parameter(s) to a dictionary
+    Input: Classifier name from sidebar drop down menu
+    Output: Dictionary with classifier specific parameters
+
+    '''
     params = {}
     if clf_name == "KNN":
         K = st.sidebar.slider("K", 1, 15)
@@ -54,9 +61,13 @@ def add_parameter_ui(clf_name):
         params["n_estimators"] = n_estimators
     return params
 
-params = add_parameter_ui(classifier_name)
-
 def get_classifier(clf_name, params):
+    '''
+    Applies parameters to respective instantiated classifier object
+    Input: Classifier name and parameters dictionary
+    Output: Classifier object instance
+
+    '''
     if clf_name == "KNN":
         clf = KNeighborsClassifier(n_neighbors=params["K"])
 
@@ -68,41 +79,49 @@ def get_classifier(clf_name, params):
                                     max_depth=params["max_depth"], random_state=1234)
     return clf
 
-clf = get_classifier(classifier_name, params)
+if __name__ == "__main__":
 
-# Classification
-# split the test-train data with custom split ratio
-# test_size = st.slider("test-train-ratio:", 0.1, 0.3)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=1234)
+    X, y = get_dataset(dataset_name)
+    st.write("shape of dataset", X.shape)
+    st.write("number of classes", len(np.unique(y)))
 
-# The API for the sklearn algorithms is the same for all classifiers
-clf.fit(X_train, y_train)
-y_prediction = clf.predict(X_test)
+    params = add_parameter_ui(classifier_name)
 
-# print out the accuracy scores
-accuracy = accuracy_score(y_test, y_prediction)
-st.write(f"classifier = {classifier_name}")
-st.write(f"accuracy = {accuracy}")
+    clf = get_classifier(classifier_name, params)
 
-# Plot classification results
-# Apply PCA to have 2D representation of our results
-# specify that we want to keep 2 dimensions 
-pca = PCA(2)
-X_projected = pca.fit_transform(X) # unsupervised, so does not need y here
+    # Classification
+    # split the test-train data with custom split ratio
+    # test_size = st.slider("test-train-ratio:", 0.1, 0.3)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=1234)
 
-x1 = X_projected[:, 0]
-x2 = X_projected[:, 1]
+    # The API for the sklearn algorithms is the same for all classifiers
+    clf.fit(X_train, y_train)
+    y_prediction = clf.predict(X_test)
 
-# create figure object
-fig = plt.figure(figsize=(4,3)) 
-# color based on labels, alpha value for transparency
-plt.scatter(x1, x2, c=y, alpha=0.8, cmap="viridis") 
-plt.xlabel("Principle Component 1")
-plt.ylabel("Principle Component 2")
-plt.colorbar()
+    # print out the accuracy scores
+    accuracy = accuracy_score(y_test, y_prediction)
+    st.write(f"classifier = {classifier_name}")
+    st.write(f"accuracy = {accuracy}")
 
-# plt.show()   # this is what we would usually need to plot
-st.pyplot(fig)
+    # Plot classification results
+    # Apply PCA to have 2D representation of our results
+    # specify that we want to keep 2 dimensions 
+    pca = PCA(2)
+    X_projected = pca.fit_transform(X) # unsupervised, so does not need y here
+
+    x1 = X_projected[:, 0]
+    x2 = X_projected[:, 1]
+
+    # create figure object
+    fig = plt.figure(figsize=(4,3)) 
+    # color based on labels, alpha value for transparency
+    plt.scatter(x1, x2, c=y, alpha=0.8, cmap="viridis") 
+    plt.xlabel("Principle Component 1")
+    plt.ylabel("Principle Component 2")
+    plt.colorbar()
+
+    # plt.show()   # this is what we would usually need to plot
+    st.pyplot(fig)
 
 # TODO
 # - add more parameters (sklearn)
