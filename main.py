c74dacebadbb75fe00a3e848a2b91d9d6a5302d1
patrickobrien1501 <jpobrien@gmx.com>
2022-04-05 16:48:59 +0200

    updated sidebar

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.write("Which one is the best classifier?")
 
 dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine"))
 
-dataset_name = st.sidebar.selectbox("Select Classifier", ("KNN", "SVM", "Random Forest"))
+classifier_name = st.sidebar.selectbox("Select Classifier", ("KNN", "SVM", "Random Forest"))
 
 # function to get the datasets
 def get_dataset(dataset_name):
@@ -29,3 +29,20 @@ def get_dataset(dataset_name):
 X, y = get_dataset(dataset_name)
 st.write("shape of dataset", X.shape)
 st.write("number of classes", len(np.unique(y)))
+
+def add_parameter_ui(clf_name):
+    params = {}
+    if clf_name == "KNN":
+        K = st.sidebar.slider("K", 1, 15)
+        params["K"] = K
+    elif clf_name == "SVM":
+        C = st.sidebar.slider("C", 0.01, 10.0)
+        params["C"] = C
+    else:
+        max_depth = st.sidebar.slider("max_depth", 2, 15)
+        n_estimators = st.sidebar.slider("n_estimators", 1, 100)
+        params["max_depth"] = max_depth
+        params["n_estimators"] = n_estimators
+    return params
+
+add_parameter_ui(classifier_name)
